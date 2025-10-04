6af44ae4eaa6d88969f351b61e12241312d0142b
orzAtalod <1759978497@qq.com>
2025-10-04 14:53:31 +0800

    feat: Set up framework and run initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use yew::prelude::*;
+
+#[derive(PartialEq, Properties)]
+pub struct CircleTestProps {}
+
+#[function_component]
+pub fn CircleTest(props: &CircleTestProps) -> Html {
+    let CircleTestProps {} = props;
+    html! {
+        <div>
+            <svg width="200" height="200">
+                <circle cx="100" cy="100" r="50" fill="blue" />
+            </svg>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<CircleTest>::with_props(CircleTestProps {}).render();
 }
